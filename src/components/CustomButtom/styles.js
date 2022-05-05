0415b0a5aded21e68684716dcb4eb8236b0fc4f9
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-05-05 12:08:45 -0300

    praticando componente Button

---
+++ src/components/CustomButtom/styles.js
@@ -0,0 +1,20 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { purple } from '@mui/material/colors';
+
+export const BootstrapButton = styled(Button)({
+  height: 60,
+  width: 150,
+  '&:hover': {
+    opacity: 0.1
+  }
+  });
+  
+export const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  }));
+  
