dddd43a7cceb750b3483e72ff88552673e106982
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-05-05 09:00:37 -0300

    importando elemento Steper

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,10 +2,11 @@ import './styles.css';
 import CustomCard from '../../components/CustomCard'
 import CustomTable from '../../components/CustomTable';
 import CustomAutoComplete from '../../components/CustomAutoComplete'
+import CustomSteper from '../../components/CustomSteper'
 function Main() {
   return (
     <div className="container-main">
-     <CustomAutoComplete />
+     <CustomSteper />
     </div>
   );
 }
