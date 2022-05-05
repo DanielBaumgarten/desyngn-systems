dc24f523e36a2e64cd53ea363812304ca8f745e8
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-05-05 10:13:40 -0300

    importando componente Dialog Form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,10 +3,12 @@ import CustomCard from '../../components/CustomCard'
 import CustomTable from '../../components/CustomTable';
 import CustomAutoComplete from '../../components/CustomAutoComplete'
 import CustomSteper from '../../components/CustomSteper'
+import AddUser from '../../components/AddUser'
+
 function Main() {
   return (
     <div className="container-main">
-     <CustomSteper />
+     <AddUser />
     </div>
   );
 }
