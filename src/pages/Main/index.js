aea6564e9e4cd2314c94e3b12590754f9880433b
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-05-05 10:41:12 -0300

    praticando e realizando alterações no Dialog

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,36 @@
+import Button from '@mui/material/Button';
 import './styles.css';
 import CustomCard from '../../components/CustomCard'
 import CustomTable from '../../components/CustomTable';
 import CustomAutoComplete from '../../components/CustomAutoComplete'
 import CustomSteper from '../../components/CustomSteper'
-import AddUser from '../../components/AddUser'
+import AddUserDialog from '../../components/AddUserDialog'
+import {useState} from 'react';
 
 function Main() {
+  
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+    const handleClickOpen = () => {
+    setOpen(true);
+  };
+
   return (
-    <div className="container-main">
-     <AddUser />
+    <div className="container-main">      
+      <Button 
+        variant="contained"
+        onClick={handleClickOpen}        
+      >
+        Cadastrar Usuário
+      </Button>
+     <AddUserDialog 
+      open={open}
+      handleClose={handleClose}
+     />
     </div>
   );
 }
