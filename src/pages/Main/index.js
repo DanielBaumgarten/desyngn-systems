78faa16f12098f0a17ee7d58fadc1db617b1cfcf
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-05-05 08:18:16 -0300

    importando nova dependência para que fosse possivel a sua utilização

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,11 @@
 import './styles.css';
 import CustomCard from '../../components/CustomCard'
-
+import CustomTable from '../../components/CustomTable';
 function Main() {
   return (
     <div className="container-main">
      <CustomCard />
+     <CustomTable />
     </div>
   );
 }
